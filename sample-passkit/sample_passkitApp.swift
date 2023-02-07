d6f476bb5d25b3b8613863a492c2cb600f66ed72
Gustavo Nogales <gustavo.nogales@hotmail.com>
2023-02-07 14:40:32 -0300

    Initial Commit

---
+++ sample-passkit/sample_passkitApp.swift
@@ -0,0 +1,17 @@
+//
+//  sample_passkitApp.swift
+//  sample-passkit
+//
+//  Created by Gustavo Nogales on 07/02/23.
+//
+
+import SwiftUI
+
+@main
+struct sample_passkitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
